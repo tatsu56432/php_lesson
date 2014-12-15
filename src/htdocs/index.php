18146a80bf90ebd5c3cbc488d877fe42590bb025
root <root@localhost.localdomain>
2014-12-15 03:15:51 +0000

    三項演算子を使ってみよう！

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -2,17 +2,12 @@
 //条件分岐
 //比較演算子　<> <= => == !=
 // &&(AND) ||(OR) !(NOT)
-$age = 10;
-if($age > 20 ){
-    echo "成人式！";
-}elseif($age > 12){
-    echo "子供";
-}else{
-    echo "幼児";
-}
+$age = 20;
 /*
-HTMLの中に書くときにhtmlと混じって煩わしいときに使う
-if () :
-else:
-endif:
+if($age > 20 ){
+    $message = "大人！";
+}else{
+    $message = "子供！";
+}
 */
+$message = ($age > 20) ? "大人！":"子供！";
