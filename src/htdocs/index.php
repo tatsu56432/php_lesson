55e0e6c98a74822f13f7ca2d90b3623eb555a571
t_nakajima <t_nakajima@bbmedia.co.jp>
2014-12-15 05:53:35 +0000

    ループを抜けてみよう

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,12 +1,21 @@
 <?php
 //ループ（while）
 $i=0;
+//ループ(for/while)
+
+//break; continue;
 /*
-while($i<10){//前判定
-    echo "$i";
-    $i++;
+for($i=0 ; $i < 10 ; $i++ ){
+    if($i>5){
+        break;
+    }
+echo $i;
 }
 */
-for($i=0 ; $i < 10 ; $i++ ){
-echo $i;
+
+for($i = 0 ; $i < 10 ; $i++){
+    if($i % 2 == 0){
+        continue;
+    }
+    echo $i;
 }
