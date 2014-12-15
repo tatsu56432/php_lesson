26f82922e1f88c58a1fe6650ddf9a0528bfcf31d
t_nakajima <t_nakajima@bbmedia.co.jp>
2014-12-15 11:08:31 +0000

    ファイルからデータを読み出そう(1)

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -3,26 +3,14 @@
 //ファイルの読み書き
 
 $testFile = "test.dat";
-$contents = "怒涛のコンツェルト";
 
-if (is_writable($testFile)){
-    //ファイルをオープンできたか？
-    //$fp = fopen($testFile,"a");
-    if(!$fp = fopen($testFile,"a")){
-        echo"could not open!";
-        exit;
-    }
-    //書き込めたか？
-   if (fwrite($fp,$contents)===false){
-       echo"could not write!";
-       exit;
-   }
-    //終了処理
-    echo "success!";
-    fclose($fp);
-}else{
-    echo"not writable!";
+if (!$fp = fopen($testFile,"r")){
+    echo "could not open";
     exit;
 }
 
+$contents = fread($fp,filesize($testFile));
 
+var_dump($contents);
+
+fclose($fp);
