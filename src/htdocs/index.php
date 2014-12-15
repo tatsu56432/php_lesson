e0dfbcb70d9430af9c3e9661bc7afa2a36721326
root <root@localhost.localdomain>
2014-12-15 02:49:08 +0000

    条件分岐をしてみよう（2）

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -2,7 +2,17 @@
 //条件分岐
 //比較演算子　<> <= => == !=
 // &&(AND) ||(OR) !(NOT)
-$age = 18;
-if($age == 20){
+$age = 10;
+if($age > 20 ){
     echo "成人式！";
+}elseif($age > 12){
+    echo "子供";
+}else{
+    echo "幼児";
 }
+/*
+HTMLの中に書くときにhtmlと混じって煩わしいときに使う
+if () :
+else:
+endif:
+*/
