52382abba893b3a51024b82dfb85379955c40202
t_nakajima <t_nakajima@bbmedia.co.jp>
2014-12-15 04:43:50 +0000

    whilwでループを回してみよう

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,19 +1,14 @@
 <?php
-//条件分岐(switch)
-
-$signal = "yellow";
-
-switch($signal) {
-    case"red":
-        echo "止まれ！";
-        break;
-    case"blue":
-        echo "進め！";
-        break;
-    case"yellow":
-        echo "注意！";
-        break;
-    default;
-        //例外処理
-        break;
+//ループ（while）
+$i=0;
+/*
+while($i<10){//前判定
+    echo "$i";
+    $i++;
 }
+*/
+
+do{
+    echo "$i";
+    $i++;
+}while($i<10);//後判定
